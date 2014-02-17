0fd9eb070843b2149ece66e05a99a9d9e55fbf8e
Maxim Kalachev <maxkalachev@yandex.ru>
2014-02-17 11:45:07 +0400

    global externs

--- externs/browser/global.js
+++ externs/browser/global.js
@@ -14,3 +14,28 @@ function btoa(str) {}
  */
 function atob(str) {}
 
+
+/**
+ * @type {!Object}
+ */
+var localStorage = {};
+
+
+/**
+ * @param {string} key Ключ.
+ * @return {string} Элемент хранилища.
+ */
+localStorage.getItem = function(key) {};
+
+
+/**
+ * @param {string} key Ключ.
+ * @param {string} value Данные.
+ */
+localStorage.setItem = function(key, value) {};
+
+
+/**
+ * @param {string} key Ключ.
+ */
+localStorage.removeItem = function(key) {};
