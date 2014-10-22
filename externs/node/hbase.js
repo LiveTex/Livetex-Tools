d553a1cb2de927c273d96bddbbb393565fffb169
Valeria Lepina <divergence082@gmail.com>
2014-10-22 14:52:59 +0400

    hbase externs

--- externs/node/hbase.js
+++ externs/node/hbase.js
@@ -38,9 +38,10 @@ hbase.Client = function(options) {};
 /**
  * @param {string} table
  * @param {string} row
+ * @param {number=} opt_timeout
  * @return {!hbase.Row}
  */
-hbase.Client.prototype.getRow = function(table, row) {};
+hbase.Client.prototype.getRow = function(table, row, opt_timeout) {};
 
 
 /**
@@ -56,20 +57,23 @@ hbase.Row = function(client, table, row) {};
  * @param {(string|!Array.<string>)} columns
  * @param {(string|!Array.<string>)} values
  * @param {function(hbase.Error)} callback
+ * @param {number=} opt_timeout
  */
-hbase.Row.prototype.put = function(columns, values, callback) {};
+hbase.Row.prototype.put = function(columns, values, callback, opt_timeout) {};
 
 
 /**
  * @param {(string|!Array.<string>)} columns
  * @param {function(hbase.Error)} callback
+ * @param {number=} opt_timeout
  */
-hbase.Row.prototype.delete = function(columns, callback) {};
+hbase.Row.prototype.delete = function(columns, callback, opt_timeout) {};
 
 
 /**
  * @param {string} column
  * @param {!Object.<string>} options
  * @param {function(hbase.Error, Array.<{column:string, $:string}>)} callback
+ * @param {number=} opt_timeout
  */
-hbase.Row.prototype.get = function(column, options, callback) {};
+hbase.Row.prototype.get = function(column, options, callback, opt_timeout) {};
